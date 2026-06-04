3ac72f10363c710e9d775a7302619de63cbd9cc8
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2026-06-04 17:37:30 +0300

    add app typography

---
+++ lib/core/theme/app_typography.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTypography {
+  static final TextStyle headlineLarge =  GoogleFonts.inter(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    height: 1.2
+  );
+  static final TextStyle headlineMedium =  GoogleFonts.inter(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    height: 1.2
+  );
+  static final TextStyle bodyLarge =  GoogleFonts.inter(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    height: 1.5
+  );
+  static final TextStyle bodyMedium =  GoogleFonts.inter(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    height: 1.5
+  );
+}
