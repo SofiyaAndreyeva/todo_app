f13a0731f5e935f1b7e1153cb19b5c9ad87c1800
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2026-06-04 17:41:14 +0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/my_app.dart';
+import 'core/core.dart';
 
 void main() {
+  setupLocator();
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage()
-    );
-  }
-}
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-
 }
